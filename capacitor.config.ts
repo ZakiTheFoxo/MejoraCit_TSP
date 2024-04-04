17fd0e8b602d7eeb7c7f53fadf1ff7f28e9b682d
ZakiTheFoxo <omararturo.diazalarcon@upaep.edu.mx>
2024-04-03 22:57:03 -0600

    build and sync to android and ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'mejora.cit',
   appName: 'mejora-cit',
-  webDir: 'www',
+  webDir: 'src',
   server: {
     androidScheme: 'https'
   }
